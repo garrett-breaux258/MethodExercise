8e65765f05c15b16c010508581a9915c6f2081a8
Garrett Breaux <garrett.breaux1@gmail.com>
2021-06-17 09:36:44 -0500

    Method Exercise Draft

--- MethodExercise/Program.cs
+++ MethodExercise/Program.cs
@@ -4,59 +4,198 @@ namespace MethodExercise
 {
     class Program
     {
+        //___________Exercise 2_________________
+
+        public static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public static int Difference(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+
+        public static int Multiply(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
+        public static int Division(int num1, int num2)
+        {
+            return num1 / num2;
+        }
+
+        public static decimal Modulus(decimal num1, decimal num2)
+        {
+            return num1 % num2;
+
+        }
+
+        public static int Sum(params int[] numbers)
+        {
+            var sum = 0;
+            foreach (var num in numbers)
+            {
+                sum = sum + num;
+
+            }
+            return sum;
+        }
+        
+        //public static int Sum(params int[] numbers)
+        //{
+        //  var sum = 0;
+        //  foreach(var num in numbers)
+        //  {
+        //      sum = sum + num;
+        //   }
+        //      return sum;
+        //}
+
+        //public static in Sum(params int[] numbers)
+        //{
+        //  Var sum = 0
+        //  foreach(var num in numbers)
+        //  {
+        //      sum = sum + num;
+        //   }
+        //      return sum;
+        //}
+
+        //public static int Sum(params int[] numbers)
+        //{
+        //  var sum = 0;
+        //  foreach(var num in numbers)
+        //  {
+        //      sum = sum + num;
+        //   }
+        //      return sum;
+        //}
+
+        //public static int Sum(params int[] numbers)
+        //{
+        //  var sum = 0;
+        //  foreach(var num in numbers)
+        //  {
+        //      sum = sum + num;
+        //   }
+        //      return sum;
+        //}
+
+        public static void SecurityQuestion1(string userName)
+        {
+
+            do
+            {
+                if (userName.ToUpper() == "GARRETT")
+                { 
+                    Console.WriteLine($"What is your favorite animal?");
+
+                }
+                else
+                {
+                    Console.WriteLine($"Only Garrett can access this computer");
+                    Console.WriteLine("Enter your name again");
+                    userName = Console.ReadLine();
+                }
+
+            } while (userName.ToUpper() != "GARRETT");
+            
+
+            
+        }
+        public static void AnimalSecurityQuestion2(string favoriteAnimal)
+        {
+            do
+            {
+                if (favoriteAnimal.ToUpper() == "MONKEY")
+                {
+                    Console.WriteLine($"What is your favorite color?");
+                }
+                else
+                {
+                    Console.WriteLine($"You can't sit with us anymore!");
+                    Console.WriteLine($"But you can try again.");
+                    favoriteAnimal = Console.ReadLine();
+                }
+
+            } while (favoriteAnimal.ToUpper() != "MONKEY");
+                
+                
+        }
+        public static void ColorSecurityQuestion3(string favoriteColor)
+        {
+            do
+            {
+                if (favoriteColor.ToUpper() == "BLACK")
+                {
+                    Console.WriteLine($"Dude,it's the best color, but final question." +
+                                        $"What is your favorite band?");
+                }
+                else
+                {
+                    Console.WriteLine("Better luck next time.");
+                    Console.WriteLine($"Try again.");
+                    favoriteColor = Console.ReadLine();
+                }
+                
+            } while (favoriteColor.ToUpper() != "BLACK");
+
+        }
+        public static void BandSecurityQuestion4(string favoriteBand)
+        {
+            do
+            {
+                if (favoriteBand.ToUpper() == "STATE CHAMPS")
+                {
+                    Console.WriteLine($"{favoriteBand} is best of all time");
+                }
+                else
+                {
+                    Console.WriteLine("You were so close");
+                    Console.WriteLine($"Try again.");
+                    favoriteBand = Console.ReadLine();
+                }
+              
+            } while (favoriteBand.ToUpper() != "STATE CHAMPS");
+
+        }
         static void Main(string[] args)
         {
+            Console.WriteLine(Sum(5, 10, 12, 24));
+            Console.WriteLine(Add(12, 10));
+            Console.WriteLine(Difference(45, 10));
+            Console.WriteLine(Multiply(2, 4));
+            Console.WriteLine(Division(16, 4));
+            Console.WriteLine($" This is modulus: {Modulus(35.2m, 2)}");
+
+
+            //-----------------Exerise 1-------------------
+
             Console.WriteLine($"Hello, what is your name?");
 
             string userName = Console.ReadLine();
 
-            if(userName == "Garrett")
-            {
-                Console.WriteLine($"Hey Garrett, what's your favorite animal?");
-            }
-            else
-            {
-                Console.WriteLine($"Only Garrett can access this computer");
+            SecurityQuestion1(userName);
 
-            }
-
+          
             string favoriteAnimal = Console.ReadLine();
 
-            if (favoriteAnimal == "Monkey")
-            {
-                Console.WriteLine($"Bro my favorite animal is also a {favoriteAnimal}," +
-                    $"tell me what your favorite color is?");
-            }
-            else
-            {
-                Console.WriteLine($"You can't sit with us anymore!");
-
-            }
+            AnimalSecurityQuestion2(favoriteAnimal);
+        
             string favoriteColor = Console.ReadLine();
-            if (favoriteColor=="Black")
-            {
-                Console.WriteLine($"Dude,it's the best color, but final question." +
-                    $"What is your favorite band?");
-            }
-            else
-            {
-                Console.WriteLine("Better luck next time.");
 
-            }
-                
+            ColorSecurityQuestion3(favoriteColor);
+
             string favoriteBand = Console.ReadLine();
-            if (favoriteBand == "State Champs")
-            {
-                Console.WriteLine($"{favoriteBand} is best of all time");
-            }
-            else
-            {
-                Console.WriteLine("Do you feel lucky punk, do you?");
-            }
 
+            BandSecurityQuestion4(favoriteBand);
             Console.WriteLine($"Congrats, {userName}. You have successfully accessed this computer by entering " +
                 $"the correct answers to the security questions: {favoriteAnimal}, {favoriteColor}, and {favoriteBand}. Have a nice day!");
-            }
+
+            
+        }
 
     }
 }
